26418274cb32a1d833466337be46d6e55b2584fd
ewanklomp <ewanklomp@hotmail.com>
2019-04-29 14:46:01 +0200

    init commit

---
+++ program.py
@@ -0,0 +1,11 @@
+import neuralnetwork as nn 
+import numpy as np
+
+with np.load('mnist.npz') as data:
+    training_images = data['training_images']
+    training_labels = data['training_labels']
+
+layer_sizes = (784,5,10)
+
+net = nn.NeuralNetwork(layer_sizes)
+net.print_accuracy(training_images, training_labels)
