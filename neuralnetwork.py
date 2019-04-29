7d18038b72dceab3cb8a543f7382e817f19834de
ewanklomp <ewanklomp@hotmail.com>
2019-04-29 21:02:39 +0200

    More examples added

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -6,7 +6,7 @@ class NeuralNetwork:
         self.weights = [np.random.standard_normal(s)/s[1]**.5 for s in weight_shapes]
         self.biases = [np.zeros((s,1)) for s in layer_sizes[1:]]
 
-    def predict(self, a):
+    def feedforward(self, a):
         for w,b in zip(self.weights, self.biases):
             a = self.activation(np.matmul(w,a) + b)
         return a
@@ -17,7 +17,15 @@ class NeuralNetwork:
         return 1/(1+np.exp(-x))
 
     def print_accuracy(self, images, labels):
-        predictions = self.predict(images)
+        predictions = self.feedforward(images)
         num_correct = sum([np.argmax(a) == np.argmax(b) for a,b in zip(predictions,labels)])
         print ('{0}/{1} accuracy: {2}%'.format(num_correct, len(images), (num_correct/len(images))*100))
-    
+
+    def backprop(self, images): #to make
+        d_weights = np.dot()
+        self.weights += d_weights
+
+    def train(self, images, labels):
+        o = self.feedforward(images)
+        self.backprop(o)
+        print('training') 
--- program.py
+++ program.py
@@ -5,7 +5,12 @@ with np.load('mnist.npz') as data:
     training_images = data['training_images']
     training_labels = data['training_labels']
 
-layer_sizes = (784,5,10)
+layer_sizes = (784,24,10)
 
 net = nn.NeuralNetwork(layer_sizes)
-net.print_accuracy(training_images, training_labels)
+
+trainingrounds = 10
+#for e in range(trainingrounds):
+    #net.train(training_images, training_labels)
+    #net.print_accuracy(training_images, training_labels)
+
