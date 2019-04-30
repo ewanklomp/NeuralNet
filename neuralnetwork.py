fadadc8a36d534cd5fdebba78a69b38b4267fbfd
ewanklomp <ewanklomp@hotmail.com>
2019-04-30 14:02:32 +0200

    Working version 0.1

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -1,31 +1,72 @@
 import numpy as np
+import random
 
 class NeuralNetwork:
     def __init__(self, layer_sizes):
+        self.layer_sizes = layer_sizes
         weight_shapes = [(a,b) for a,b in zip(layer_sizes[1:], layer_sizes[:-1])]
         self.weights = [np.random.standard_normal(s)/s[1]**.5 for s in weight_shapes]
         self.biases = [np.zeros((s,1)) for s in layer_sizes[1:]]
 
     def feedforward(self, a):
         for w,b in zip(self.weights, self.biases):
-            a = self.activation(np.matmul(w,a) + b)
+            a = self.sigmoid(np.matmul(w,a) + b)
         return a
 
+    def SGD(self, images, mini_batch_size, learningrate): #stochastic gradient descent, split everything up in minibatches for efficiency
+        
+        imagecount = len(images)
+        random.shuffle(images)
+        mini_batches = [images[k:k+mini_batch_size] for k in range(0, imagecount, mini_batch_size)]
+        for mini_batch in mini_batches:
+            self.update_mini_batch(mini_batch, learningrate)
 
-    @staticmethod
-    def activation(x):
-        return 1/(1+np.exp(-x))
+        print("SGD finished")
+
+    def update_mini_batch(self, mini_batch, learningrate):
+        del_b = [np.zeros(b.shape) for b in self.biases] 
+        del_w = [np.zeros(w.shape) for w in self.weights]
+        
+        for x,y in mini_batch:
+            delta_b, delta_w = self.backprop(x,y)
+            del_b = [nb+dnb for nb, dnb in zip(del_b, delta_b)]
+            del_w = [nw+dnw for nw, dnw in zip(del_w, delta_w)]
+        
+        self.weights = [w-(learningrate/len(mini_batch))*delw for w, delw in zip(self.weights, del_w)]
+        self.biases = [b-(learningrate/len(mini_batch))*delb for b, delb in zip(self.biases, del_b)]
+
+
+    def backprop(self, x, y): #to make
+        del_b = [np.zeros(b.shape) for b in self.biases] 
+        del_w = [np.zeros(w.shape) for w in self.weights]
+        activation = x
+        activations = [x] # list to store all the activations, layer by layer
+        zs = [] # list to store all the z vectors, layer by layer
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation)+b 
+            zs.append(z)
+            activation = self.sigmoid(z)
+            activations.append(activation)
+        
+        delta = (activations[-1] - y) * (self.sigmoid(z)*(1-self.sigmoid(z))) # cost_derivative * sigmoid_prime
+        del_b[-1] = delta
+        del_w[-1] = np.matmul(delta, activations[-2].transpose())
+
+        for l in range(2, len(self.layer_sizes)):
+            z = zs[-l]
+            sp = self.sigmoid(z)*(1-self.sigmoid(z)) #sigmoid_prime of z
+            delta = np.matmul(self.weights[-l+1].transpose(), delta) * sp
+            del_b[-l] = delta
+            del_w[-l] = np.matmul(delta, activations[-l-1].transpose())
+        return (del_b, del_w)
+
 
     def print_accuracy(self, images, labels):
         predictions = self.feedforward(images)
         num_correct = sum([np.argmax(a) == np.argmax(b) for a,b in zip(predictions,labels)])
         print ('{0}/{1} accuracy: {2}%'.format(num_correct, len(images), (num_correct/len(images))*100))
 
-    def backprop(self, images): #to make
-        d_weights = np.dot()
-        self.weights += d_weights
+    @staticmethod
+    def sigmoid(x):
+        return 1/(1+np.exp(-x))
 
-    def train(self, images, labels):
-        o = self.feedforward(images)
-        self.backprop(o)
-        print('training') 
--- program.py
+++ program.py
@@ -5,12 +5,16 @@ with np.load('mnist.npz') as data:
     training_images = data['training_images']
     training_labels = data['training_labels']
 
-layer_sizes = (784,24,10)
+layer_sizes = (784,16,10)
+trainingrounds = 10
+mini_batch_size = 20
+learning_rate = 0.1
+
+training_data = list(zip(training_images, training_labels))
 
 net = nn.NeuralNetwork(layer_sizes)
 
-trainingrounds = 10
-#for e in range(trainingrounds):
-    #net.train(training_images, training_labels)
-    #net.print_accuracy(training_images, training_labels)
+for e in range(trainingrounds):
+    net.SGD(training_data, mini_batch_size, learning_rate)
+    net.print_accuracy(training_images, training_labels)
 
